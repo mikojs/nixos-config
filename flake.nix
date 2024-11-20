7a8fb0734185b70b533a38eda5cecc659caecd13
HsuTing <hsuting0106@gmail.com>
2024-11-20 17:04:22 +0000

    feat(flxke): add formatter

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
       stateVersion = "24.05";
     };
   in {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+
     nixosConfigurations.wsl = makeSystem {
       system = "x86_64-linux";
       wsl = true;
