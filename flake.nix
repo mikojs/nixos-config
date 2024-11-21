af51f74ce8eed111091e38549fc4aede9da2083f
HsuTing <hsuting0106@gmail.com>
2024-11-21 16:08:59 +0000

    refactor(flake): remove home manager

--- flake.nix
+++ flake.nix
@@ -8,11 +8,6 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { nixpkgs, ... }@inputs:
