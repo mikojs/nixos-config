5b4ba30ddfffa33b1ddb0fbc8d081c0d231ebede
HsuTing <hsuting0106@gmail.com>
2024-11-25 07:38:45 +0000

    feat(home manager): add fish theme

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -1,9 +1,23 @@
-{
+{ pkgs
+, ...
+}: {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       test -f ~/init.fish && source ~/init.fish 2> /dev/null
     '';
+
+    plugins = [
+      {
+        name = "agnoster";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "theme-agnoster";
+          rev = "master";
+          sha256 = "OFESuesnfqhXM0aij+79kdxjp4xgCt28YwTrcwQhFMU=";
+        };
+      }
+    ];
   };
 }
