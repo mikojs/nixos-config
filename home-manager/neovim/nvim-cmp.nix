7c22f1deac5941cb7b43411353b0b985ebfb496c
HsuTing <hsuting0106@gmail.com>
2024-12-10 00:30:45 +0000

    fix(neovim): fix plugin

--- home-manager/neovim/nvim-cmp.nix
+++ home-manager/neovim/nvim-cmp.nix
@@ -25,8 +25,11 @@
           cmp.setup({
             sources = cmp.config.sources({
               { name = 'nvim_lsp' },
+              { name = 'vsnip' },
               { name = 'path' },
               { name = 'nvim_treesitter' },
+            }, {
+              { name = 'buffer' },
             }),
             snippet = {
               expand = function(args)
@@ -37,7 +40,13 @@
               completion = cmp.config.window.bordered(),
               documentation = cmp.config.window.bordered(),
             },
-            mapping = cmp.mapping.preset.insert({})
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            })
           })
 
           cmp.setup.cmdline({ '/', '?' }, {
