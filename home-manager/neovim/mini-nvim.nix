1fe62523efbf42352b83b1a272eede52cdfd54fb
HsuTing <hsuting0106@gmail.com>
2024-12-17 17:25:07 +0000

    feat(mini-nvim): add more plugin

--- home-manager/neovim/mini-nvim.nix
+++ home-manager/neovim/mini-nvim.nix
@@ -8,7 +8,7 @@
       plugin = mini-icons;
       config = ''
         lua << END
-          require('mini.icons').setup({});
+          require('mini.icons').setup();
         END
       '';
     }
@@ -17,7 +17,34 @@
       plugin = mini-ai;
       config = ''
         lua << END
-          require('mini.ai').setup({});
+          require('mini.ai').setup();
+        END
+      '';
+    }
+
+    {
+      plugin = mini-comment;
+      config = ''
+        lua << END
+          require('mini.comment').setup();
+        END
+      '';
+    }
+
+    {
+      plugin = mini-surround;
+      config = ''
+        lua << END
+          require('mini.surround').setup();
+        END
+      '';
+    }
+
+    {
+      plugin = mini-notify;
+      config = ''
+        lua << END
+          require('mini.notify').setup();
         END
       '';
     }
