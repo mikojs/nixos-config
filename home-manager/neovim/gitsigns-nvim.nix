463b1705859f945c19e7446f02729b665f9e7614
HsuTing <hsuting0106@gmail.com>
2024-12-22 07:54:02 +0000

    feat(gitsigns): add config

--- home-manager/neovim/gitsigns-nvim.nix
+++ home-manager/neovim/gitsigns-nvim.nix
@@ -4,6 +4,19 @@
 }:
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
-    gitsigns-nvim
+    {
+      plugin = gitsigns-nvim;
+      config = ''
+        lua << END
+          require('gitsigns').setup({
+            numhl = true,
+            current_line_blame = true,
+            current_line_blame_opts = {
+              delay = 100,
+            },
+          })
+        END
+      '';
+    }
   ];
 }
