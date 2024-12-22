2eff2a426a510f16dde7353211815836f135f6dc
HsuTing <hsuting0106@gmail.com>
2024-12-22 19:20:26 +0000

    feat(telescope): use which-keys

--- home-manager/neovim/telescope-nvim.nix
+++ home-manager/neovim/telescope-nvim.nix
@@ -14,11 +14,21 @@
       config = ''
         lua << END
           local builtin = require('telescope.builtin')
+          local wk = require('which-key')
 
-          vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-          vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-          vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-          vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+          wk.add({
+            { '<leader>t', group = 'Telescope' },
+            { '<leader>tf', builtin.find_files, desc = 'Telescope find files' },
+            { '<leader>tl', builtin.live_grep, desc = 'Telescope live grep' },
+            { '<leader>tb', builtin.buffers, desc = 'Telescope buffers' },
+            { '<leader>th', builtin.help_tags, desc = 'Telescope help tags' },
+            { '<leader>tk', builtin.keymaps, desc = 'Telescope keymaps' },
+
+            { '<leader>tg', group = 'Telescope git' },
+            { '<leader>tgs', builtin.git_status, desc = 'Telescope git status' },
+            { '<leader>tgh', builtin.git_stash, desc = 'Telescope git stash' },
+            { '<leader>tgc', builtin.git_commits, desc = 'Telescope git commit' },
+          })
         END
       '';
     }
