2a952370dd4b9faf4b80e93a1e16297be5d00754
HsuTing <hsuting0106@gmail.com>
2024-12-25 17:48:50 +0000

    feat(toggleterm-nvim): add key map

--- home-manager/neovim/toggleterm-nvim.nix
+++ home-manager/neovim/toggleterm-nvim.nix
@@ -7,7 +7,19 @@
     {
       plugin = toggleterm-nvim;
       config = ''
-        require("toggleterm").setup()
+        lua << END
+          local wk = require("which-key")
+
+          require("toggleterm").setup()
+          wk.add({
+            { "<leader>T", group = "Toggleterm" },
+            { "<leader>Tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
+            { "<leader>Tv", "<cmd>ToggleTerm direction=vertical size=60<cr>", desc = "Toggle vertical terminal" },
+            { "<leader>Th", "<cmd>ToggleTerm<cr>", desc = "Toggle horizontal terminal" },
+            { "<leader>Tn", "<cmd>ToggleTerm direction=tab<cr>", desc = "Toggle tab terminal" },
+            { "<esc>", "<C-\\><C-n>", desc = "Exit terminal mode", mode = "t" },
+          })
+        END
       '';
     }
   ];
