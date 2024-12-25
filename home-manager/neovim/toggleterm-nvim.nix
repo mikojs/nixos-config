2aab8f26a6b4c99b959ed4f32a0a877884a4caf0
HsuTing <hsuting0106@gmail.com>
2024-12-25 19:30:52 +0000

    feat(toggleterm-nvim): could send code to terminal

--- home-manager/neovim/toggleterm-nvim.nix
+++ home-manager/neovim/toggleterm-nvim.nix
@@ -9,14 +9,20 @@
       config = ''
         lua << END
           local wk = require("which-key")
+          local toggleterm = require("toggleterm")
 
-          require("toggleterm").setup()
+          local send_selection_to_terminal = function()
+            toggleterm.send_lines_to_terminal("visual_selection", true, { args = vim.v.count })
+          end
+
+          toggleterm.setup()
           wk.add({
             { "<leader>T", group = "Toggleterm" },
             { "<leader>Tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
             { "<leader>Tv", "<cmd>ToggleTerm direction=vertical size=60<cr>", desc = "Toggle vertical terminal" },
             { "<leader>Th", "<cmd>ToggleTerm<cr>", desc = "Toggle horizontal terminal" },
             { "<leader>Tn", "<cmd>ToggleTerm direction=tab<cr>", desc = "Toggle tab terminal" },
+            { "<leader>s", send_selection_to_terminal, desc = "Send selection to terminal", mode = "v" },
             { "<esc>", "<C-\\><C-n>", desc = "Exit terminal mode", mode = "t" },
           })
         END
