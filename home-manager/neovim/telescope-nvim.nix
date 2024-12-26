9652d9e5822e3ece711cfb9b3fdf3a10a172847b
HsuTing <hsuting0106@gmail.com>
2024-12-26 06:09:20 +0000

    fix(telescope-nvim): support to select multiple files

--- home-manager/neovim/telescope-nvim.nix
+++ home-manager/neovim/telescope-nvim.nix
@@ -13,8 +13,46 @@
       plugin = telescope-nvim;
       config = ''
         lua << END
-          local builtin = require('telescope.builtin')
           local wk = require('which-key')
+          local builtin = require('telescope.builtin')
+
+          local single_or_multi_select = function(prompt_bufnr)
+            local current_picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+            local has_multi_selection = next(current_picker:get_multi_selection()) ~= nil
+
+            if has_multi_selection then
+              local results = {}
+
+              require('telescope.actions.utils').map_selections(prompt_bufnr, function(selection)
+                table.insert(results, selection[1])
+              end)
+
+              for _, filepath in ipairs(results) do
+                vim.cmd.badd({ args = { filepath } })
+              end
+
+              require("telescope.pickers").on_close_prompt(prompt_bufnr)
+
+              if vim.fn.bufname() == "" and not vim.bo.modified then
+                vim.cmd.bwipeout()
+                vim.cmd.buffer(results[1])
+              end
+
+              return
+            end
+
+            require("telescope.actions").file_edit(prompt_bufnr)
+          end
+
+          require('telescope').setup({
+            defaults = {
+              mappings = {
+                i = {
+                  ['<cr>'] = single_or_multi_select,
+                },
+              },
+            },
+          })
 
           wk.add({
             { '<leader>T', group = 'Telescope' },
