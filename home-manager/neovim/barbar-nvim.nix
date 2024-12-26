3ec8e03b8c2ff9411230a293dcc1c2d2710a01f7
HsuTing <hsuting0106@gmail.com>
2024-12-26 03:48:25 +0000

    feat(barbar-nvim): add desc

--- home-manager/neovim/barbar-nvim.nix
+++ home-manager/neovim/barbar-nvim.nix
@@ -14,31 +14,32 @@
           local opts = { noremap = true, silent = true }
 
           wk.add({
-            { '<A-[>', '<Cmd>BufferPrevious<CR>', opts },
-            { '<A-]>', '<Cmd>BufferNext<CR>', opts },
+            { '<A-[>', '<Cmd>BufferPrevious<CR>', opts, desc = "Go to previous tab" },
+            { '<A-]>', '<Cmd>BufferNext<CR>', opts, desc = "Go to next tab" },
 
-            { '<A-,>', '<Cmd>BufferMovePrevious<CR>', opts },
-            { '<A-.>', '<Cmd>BufferMoveNext<CR>', opts },
+            { '<A-,>', '<Cmd>BufferMovePrevious<CR>', opts, desc = "Move tab left" },
+            { '<A-.>', '<Cmd>BufferMoveNext<CR>', opts, desc = "Move tab right" },
 
-            { '<A-1>', '<Cmd>BufferGoto 1<CR>', opts },
-            { '<A-2>', '<Cmd>BufferGoto 2<CR>', opts },
-            { '<A-3>', '<Cmd>BufferGoto 3<CR>', opts },
-            { '<A-4>', '<Cmd>BufferGoto 4<CR>', opts },
-            { '<A-5>', '<Cmd>BufferGoto 5<CR>', opts },
-            { '<A-6>', '<Cmd>BufferGoto 6<CR>', opts },
-            { '<A-7>', '<Cmd>BufferGoto 7<CR>', opts },
-            { '<A-8>', '<Cmd>BufferGoto 8<CR>', opts },
-            { '<A-9>', '<Cmd>BufferGoto 9<CR>', opts },
-            { '<A-0>', '<Cmd>BufferLast<CR>', opts },
+            { '<A-1>', '<Cmd>BufferGoto 1<CR>', opts, desc = "Go to tab 1" },
+            { '<A-2>', '<Cmd>BufferGoto 2<CR>', opts, desc = "Go to tab 2" },
+            { '<A-3>', '<Cmd>BufferGoto 3<CR>', opts, desc = "Go to tab 3" },
+            { '<A-4>', '<Cmd>BufferGoto 4<CR>', opts, desc = "Go to tab 4" },
+            { '<A-5>', '<Cmd>BufferGoto 5<CR>', opts, desc = "Go to tab 5" },
+            { '<A-6>', '<Cmd>BufferGoto 6<CR>', opts, desc = "Go to tab 6" },
+            { '<A-7>', '<Cmd>BufferGoto 7<CR>', opts, desc = "Go to tab 7" },
+            { '<A-8>', '<Cmd>BufferGoto 8<CR>', opts, desc = "Go to tab 8" },
+            { '<A-9>', '<Cmd>BufferGoto 9<CR>', opts, desc = "Go to tab 9" },
+            { '<A-0>', '<Cmd>BufferLast<CR>', opts, desc = "Go to last tab" },
 
-            { '<A-p>', '<Cmd>BufferPin<CR>', opts },
-            { '<A-c>', '<Cmd>BufferClose<CR>', opts },
+            { '<A-p>', '<Cmd>BufferPin<CR>', opts, desc = "Pin tab" },
+            { '<A-c>', '<Cmd>BufferClose<CR>', opts, desc = "Close tab" },
 
-            { '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts },
-            { '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts },
-            { '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts },
-            { '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts },
-            { '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts },
+            { '<leader>b', group = "Tab (Barbar)" },
+            { '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts, desc = "Sort by buffer number" },
+            { '<leader>bn', '<Cmd>BufferOrderByName<CR>', opts, desc = "Sort by name" },
+            { '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts, desc = "Sort by directory" },
+            { '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts, desc = "Sort by language" },
+            { '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts, desc = "Sort By window number" },
           })
         END
       '';
