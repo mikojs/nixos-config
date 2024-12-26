539240f1c61406456f72f48ae392d9e82472e388
HsuTing <hsuting0106@gmail.com>
2024-12-26 03:21:05 +0000

    perf(toggleterm): change key map

--- home-manager/neovim/toggleterm-nvim.nix
+++ home-manager/neovim/toggleterm-nvim.nix
@@ -18,10 +18,10 @@
           toggleterm.setup()
           wk.add({
             { "<leader>T", group = "Terminal (Toggleterm)" },
-            { "<leader>Tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
+            { "<leader>Tn", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
             { "<leader>Tv", "<cmd>ToggleTerm direction=vertical size=60<cr>", desc = "Toggle vertical terminal" },
             { "<leader>Th", "<cmd>ToggleTerm<cr>", desc = "Toggle horizontal terminal" },
-            { "<leader>Tn", "<cmd>ToggleTerm direction=tab<cr>", desc = "Toggle tab terminal" },
+            { "<leader>Tt", "<cmd>ToggleTerm direction=tab<cr>", desc = "Toggle tab terminal" },
             { "<leader>s", send_selection_to_terminal, desc = "Send selection to terminal", mode = "v" },
             { "<esc>", "<C-\\><C-n>", desc = "Exit terminal mode", mode = "t" },
           })
