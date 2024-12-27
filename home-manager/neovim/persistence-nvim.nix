6590196f0726484cf71fd0d0eb1a3958853a1ea9
HsuTing <hsuting0106@gmail.com>
2024-12-27 09:39:10 +0000

    feat(persistence-nvim): add key map

--- home-manager/neovim/persistence-nvim.nix
+++ home-manager/neovim/persistence-nvim.nix
@@ -11,6 +11,12 @@
           require("persistence").setup({
             need = 0,
           })
+
+          require("which-key").add({
+            { "<leader>q", group = "Session (Persistence)" },
+            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc ="Load last session" },
+            { "<leader>qd", function() require("persistence").stop() end, desc = "Stop Persistence" },
+          })
         END
       '';
     }
