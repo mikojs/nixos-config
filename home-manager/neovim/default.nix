42e399b3f8561d125ca647ee258e562a925f9a3a
HsuTing <hsuting0106@gmail.com>
2024-12-29 15:07:01 +0000

    feat(nevim): add window resize key map

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -99,7 +99,15 @@ in
       }
 
       require("which-key").add({
-        { "<leader>c", "\"+y ", desc = "Copy to clipboard", mode = "v" },
+        { "<leader>c", "\"+y", desc = "Copy to clipboard", mode = "v" },
+
+        { "<leader>w", group = "Window" },
+        { "<leader>wh", group = "Resize height" },
+        { "<leader>wh+", "<Cmd>resize +10<CR>", desc = "Increase 10 height" },
+        { "<leader>wh-", "<Cmd>resize -10<CR>", desc = "Decrease 10 height" },
+        { "<leader>ww", group = "Resize width" },
+        { "<leader>ww+", "<Cmd>vertical resize +20<CR>", desc = "Increase 20 width" },
+        { "<leader>ww-", "<Cmd>vertical resize -20<CR>", desc = "Decrease 20 width" },
       })
     '';
   };
