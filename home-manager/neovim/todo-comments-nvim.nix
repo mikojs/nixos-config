124cde30fcbaf4b29e869df2d756ae73ff4fdbcd
HsuTing <hsuting0106@gmail.com>
2024-12-29 18:56:08 +0000

    feat(todo-comments-nvim): add config

--- home-manager/neovim/todo-comments-nvim.nix
+++ home-manager/neovim/todo-comments-nvim.nix
@@ -3,7 +3,17 @@
   ...
 }:
 {
-  home.packages = with pkgs.vimPlugins; [
-    todo-comments-nvim
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    {
+      plugin = todo-comments-nvim;
+      config = ''
+        lua << END
+          require("todo-comments").setup()
+          require("which-key").add({
+            { "<leader>O", "<Cmd>TodoTelescope<CR>", desc = "Show TODOs" },
+          })
+        END
+      '';
+    }
   ];
 }
