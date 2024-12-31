22fd0717dc5a81dc3a7981d3958bac3d4ea7bce2
HsuTing <hsuting0106@gmail.com>
2024-12-31 20:01:42 +0000

    perf(nvim-cmp): use clippy to check rust code

--- home-manager/neovim/nvim-cmp/rust.nix
+++ home-manager/neovim/nvim-cmp/rust.nix
@@ -5,11 +5,19 @@
 {
   packages = with pkgs; [
     rust-analyzer
+    clippy
   ];
 
   config = ''
     require("lspconfig").rust_analyzer.setup({
       capabilities = capabilities,
+      settings = {
+        ["rust-analyzer"] = {
+          check = {
+            command = "clippy",
+          },
+        },
+      },
     })
   '';
 }
