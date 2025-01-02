959db5f3f4946885f1d9d26469b3a2423a88eb41
HsuTing <hsuting0106@gmail.com>
2025-01-02 09:19:12 +0000

    fix(custom): fix detail

--- overlays/custom/init/default.nix
+++ overlays/custom/init/default.nix
@@ -1,10 +1,18 @@
 {
-  pkgs,
   rustPlatform,
 }:
-with pkgs;
 with rustPlatform;
 buildRustPackage {
   name = "init";
   src = ./..;
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+  };
+  buildPhase = ''
+    cargo build --release -p init
+  '';
+  installPhase = ''
+    mkdir $out/bin
+    cp -r target/release/init $out/bin/init
+  '';
 }
