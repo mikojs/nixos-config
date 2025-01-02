85bea8f1b1bac0ebbf0a2c1971bd87622473e62f
HsuTing <hsuting0106@gmail.com>
2025-01-02 12:58:47 +0000

    feat(initialize): support to ask question

--- overlays/custom/initialize/src/main.rs
+++ overlays/custom/initialize/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use dialoguer::{Error as DialoguerError, FuzzySelect};
+use std::env;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MainError {
+    #[error("Dialoguer error: {0}")]
+    Dialoguer(#[from] DialoguerError),
+}
+
+struct Config {
+    tide: bool,
+}
+
+static TIDE_ITMES: &[&str] = &["Yes", "No", "Skip"];
+
+fn main() -> Result<(), MainError> {
+    let mut config = Config { tide: true };
+
+    if env::var("TIDE_INIT").is_err() {
+        let result = FuzzySelect::new()
+            .with_prompt("Do you want to initialize a new Tide configure?")
+            .items(TIDE_ITMES)
+            .interact()?;
+
+        match TIDE_ITMES[result] {
+            "Yes" => {
+                // TODO: Initialize Tide
+                config.tide = true;
+            }
+            "No" => {
+                config.tide = true;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
