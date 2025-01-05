c0a296660ac5f220a5a06f0784e54769ddaa07bc
HsuTing <hsuting0106@gmail.com>
2025-01-05 16:00:14 +0000

    fix(nixos): fix tailscale network problem

--- nixos/default.nix
+++ nixos/default.nix
@@ -18,6 +18,10 @@ with inputs;
       wsl.enable = isWSL;
       nixpkgs.config.allowUnfree = true;
       services.tailscale.enable = true;
+      boot.kernel.sysctl = {
+        "net.ipv4.ip_forward" = 1;
+        "net.ipv6.conf.all.forwarding" = 1;
+      };
     }
   ];
 }
