9867492ef8e62764572647b6f16abb9994d268ca
HsuTing <hsuting0106@gmail.com>
2025-01-05 16:38:35 +0000

    feat(nixos): add tailscale

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,11 +17,8 @@ with inputs;
       system.stateVersion = stateVersion;
       wsl.enable = isWSL;
       nixpkgs.config.allowUnfree = true;
-      services.tailscale.enable = true;
-      boot.kernel.sysctl = {
-        "net.ipv4.ip_forward" = 1;
-        "net.ipv6.conf.all.forwarding" = 1;
-      };
     }
+
+    (import ./tailscale.nix)
   ];
 }
