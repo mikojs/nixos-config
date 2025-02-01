d4bdbe120fc37ada700ec81918cf30048e9b06a3
Mikojs <mikojs@gmail.com>
2025-02-01 09:22:31 +0000

    perf(vmware): change core style

--- nixos/vmware.nix
+++ nixos/vmware.nix
@@ -2,11 +2,11 @@
   ...
 }:
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = true;
-  services.openssh.settings.PermitRootLogin = "yes";
-  users.users.root.initialPassword = "root";
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
 }
