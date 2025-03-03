f31a4f66e34e0989aa6e526fae259fa51c9fa298
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2025-03-04 02:48:40 +0800

    Add fcitx5 config (#36)

--- nixos/fcitx5.nix
+++ nixos/fcitx5.nix
@@ -6,12 +6,40 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      fcitx5-configtool
-      fcitx5-chinese-addons
-      fcitx5-chewing
-      fcitx5-material-color
-    ];
+    fcitx5 = {
+      ignoreUserConfig = true;
+
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-chinese-addons
+        fcitx5-chewing
+        fcitx5-nord
+      ];
+
+      settings = {
+        globalOptions.Behavior = {
+          resetStateWhenFocusIn = "All";
+          ShareInputState = "All";
+        };
+
+        addons.classicui.globalSection = {
+          Theme = "Nord-Dark";
+          Font = "Fira Code Medium 5";
+          MenuFont = "Fira Code Medium 5";
+          TrayFont = "Fira Code Demi-Bold 5";
+        };
+
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "chewing";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "chewing";
+          GroupOrder."0" = "Default";
+        };
+      };
+    };
   };
 }
