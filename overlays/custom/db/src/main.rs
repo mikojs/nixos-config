00fcebe9ca737e95ec6fbf58ff6a16d44d08fdb7
Mikojs <mikojs@gmail.com>
2025-03-27 23:23:11 +0000

    feat(db): add clap

--- overlays/custom/db/src/main.rs
+++ overlays/custom/db/src/main.rs
@@ -1,9 +1,17 @@
+use clap::Parser;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum MainError {}
 
+#[derive(Parser)]
+struct Cli {
+    url: String,
+}
+
 fn main() -> Result<(), MainError> {
-    println!("db");
+    let cli = Cli::parse();
+
+    println!("{}", cli.url);
     Ok(())
 }
