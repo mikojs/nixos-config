ac03b36c628df2438b4fbb8ccfbdf3a2c76593e8
HsuTing <hsuting0106@gmail.com>
2025-04-06 01:20:48 +0000

    feat(db): add clap and auto complete

--- overlays/custom/db/src/main.rs
+++ overlays/custom/db/src/main.rs
@@ -1,10 +1,48 @@
+use std::io::{self, Error as IoError};
+
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Shell};
+use config::Config;
 use thiserror::Error;
 
 mod config;
 
 #[derive(Error, Debug)]
-enum MainError {}
+enum MainError {
+    #[error("IoError: {0}")]
+    Io(#[from] IoError),
+}
+
+#[derive(Subcommand)]
+enum Commands {}
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, value_enum, hide = true)]
+    generate: Option<Shell>,
+    #[command(subcommand)]
+    commands: Option<Commands>,
+}
 
 fn main() -> Result<(), MainError> {
+    let _ = Config::new();
+    let cli = Cli::parse();
+
+    if let Some(generator) = cli.generate {
+        let cmd = &mut Cli::command();
+
+        // TODO: auto load and update when the env update or update command
+        generate(
+            generator,
+            cmd,
+            cmd.get_name().to_string(),
+            &mut io::stdout(),
+        );
+    } else {
+        match cli.commands {
+            _ => Cli::command().print_help()?,
+        }
+    }
+
     Ok(())
 }
