4c9e6db9deb0a9d1cc9d6b950b0214025214ab82
HsuTing <hsuting0106@gmail.com>
2025-04-08 19:13:39 +0000

    feat(nvim-cmp): add sqls.nvim

--- home-manager/neovim/nvim-cmp/db.nix
+++ home-manager/neovim/nvim-cmp/db.nix
@@ -7,9 +7,16 @@
     sqls
   ];
 
+  plugins = with pkgs.vimPlugins; [
+    sqls-nvim
+  ];
+
   config = ''
     require("lspconfig").sqls.setup({
       capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        require('sqls').on_attach(client, bufnr)
+      end,
     })
   '';
 }
