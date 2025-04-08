2e62cccc08c4c5eda063a8c9a5030862d5ba6634
HsuTing <hsuting0106@gmail.com>
2025-04-08 19:08:03 +0000

    feat(overlays): add sqls.nvim pkgs

--- overlays/patch.nix
+++ overlays/patch.nix
@@ -15,5 +15,18 @@ with prev.vimUtils;
       meta.homepage = "https://github.com/saecki/crates.nvim/";
       meta.hydraPlatforms = [ ];
     };
+
+    sqls-nvim = buildVimPlugin {
+      pname = "sqls.nvim";
+      version = "2025-03-27";
+      src = fetchFromGitHub {
+        owner = "nanotee";
+        repo = "sqls.nvim";
+        rev = "af156c2c7d6bca6384d0881f4a3d6de4cedcd482";
+        sha256 = "sha256-D5k8rAwC1vpdbm0MBsJ9vc2vs2Mq3e8ZTtHDycw2cYs=";
+      };
+      meta.homepage = "https://github.com/nanotee/sqls.nvim/";
+      meta.hydraPlatforms = [ ];
+    };
   };
 }
