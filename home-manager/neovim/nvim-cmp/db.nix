a39f979b866bf7ada2053c1e9c95b02b0fd5c6c7
HsuTing <hsuting0106@gmail.com>
2025-04-09 01:13:49 +0000

    feat(nvim): add db command to sqls setting

--- home-manager/neovim/nvim-cmp/db.nix
+++ home-manager/neovim/nvim-cmp/db.nix
@@ -5,6 +5,7 @@
 {
   packages = with pkgs; [
     sqls
+    miko-db
   ];
 
   plugins = with pkgs.vimPlugins; [
@@ -12,11 +13,20 @@
   ];
 
   config = ''
+    local db_sqls_command = io.popen("db sqls")
+    local connections = vim.json.decode(db_sqls_command:read("*a"))
+
+    db_sqls_command:close()
     require("lspconfig").sqls.setup({
       capabilities = capabilities,
       on_attach = function(client, bufnr)
         require('sqls').on_attach(client, bufnr)
       end,
+      settings = {
+        sqls = {
+          connections= connections,
+        },
+      },
     })
   '';
 }
