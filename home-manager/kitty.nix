1cb63203bf75d30b90496eeebaa6dab1b64af911
HsuTing <hsuting0106@gmail.com>
2025-04-16 13:57:16 +0800

    fix(home-manager): fix todo comment

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -9,7 +9,7 @@
     enable = true;
     extraConfig =
       with builtins;
-      # FIXME default shell, https://github.com/nix-darwin/nix-darwin/issues/1237
+      # FIXME: default shell, https://github.com/nix-darwin/nix-darwin/issues/1237
       (replaceStrings [ "fish" ] [ "/etc/profiles/per-user/${userName}/bin/fish" ] (readFile ./kitty));
   };
 }
