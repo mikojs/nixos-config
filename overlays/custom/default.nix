a94f292f1bbc0eb1f75680c4a1f076adb7c7d201
HsuTing <hsuting0106@gmail.com>
2025-05-02 19:06:36 +0000

    feat(custom): merge fish shell

--- overlays/custom/default.nix
+++ overlays/custom/default.nix
@@ -3,48 +3,45 @@ with prev;
 with lib;
 with builtins;
 let
+  custom-pkg-names = [
+    "initialize"
+    "db"
+  ];
+
   custom-pkgs = listToAttrs (
-    map
-      (
-        name:
-        nameValuePair "miko-${name}" (
-          with rustPlatform;
-          buildRustPackage {
-            inherit name;
-            src = ./.;
+    map (
+      name:
+      nameValuePair "miko-${name}" (
+        with rustPlatform;
+        buildRustPackage {
+          inherit name;
+          src = ./.;
 
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
 
-            buildPhase = ''
-              cargo build --release -p ${name}
-            '';
+          buildPhase = ''
+            cargo build --release -p ${name}
+          '';
 
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r target/release/${name} $out/bin
-            '';
-          }
-        )
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r target/release/${name} $out/bin
+          '';
+        }
       )
-      [
-        "initialize"
-        "db"
-      ]
+    ) custom-pkg-names
   );
 in
 custom-pkgs
 // {
-  miko-fish.interactiveShellInit = ''
-    # Initialize
-    if type -q initialize
-      initialize
-    end
-
-    # db
-    if type -q db
-      db --generate fish | source
-    end
-  '';
+  miko-fish.interactiveShellInit = concatStrings (
+    map (name: ''
+      # ${name}
+      if type -q ${name}
+        ${name} ${if name == "initialize" then "" else "--generate fish | source"}
+      end
+    '') custom-pkg-names
+  );
 }
