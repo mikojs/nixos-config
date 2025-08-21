d3baedb5e7f18b94749f73bf2439d1e628b31376
HsuTing <hsuting0106@gmail.com>
2025-08-21 01:15:51 +0000

    feat(home manager): add mcp servers

--- home-manager/ai/gemini.nix
+++ home-manager/ai/gemini.nix
@@ -6,4 +6,13 @@
   home.packages = with pkgs; [
     gemini-cli
   ];
+
+  home.file = with builtins; {
+    ".gemini/settings.json".text = ''
+      {
+        "selectedAuthType": "oauth-personal",
+        "mcpServers": ${readFile ./mcpServers.json}
+      }
+    '';
+  };
 }
