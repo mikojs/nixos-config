53c605fd6d788e54fd2cace78a6c9e1b51ef42da
HsuTing <hsuting0106@gmail.com>
2025-08-22 14:31:41 +0000

    perf(patch): copy patch from pkgs

--- overlays/patch/claude-code/default.nix
+++ overlays/patch/claude-code/default.nix
@@ -1,4 +1,3 @@
-# other files are copied from https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/cl/claude-code
 inputs: {
   claude-code = import ./packages.nix (if inputs ? lib then inputs else (import <nixpkgs> { }));
 }
