6082973c4cd4a1d0859cbc691101452b54fd364f
HsuTing <hsuting0106@gmail.com>
2025-09-08 18:26:25 +0000

    feat(n8n): add home dir

--- home-manager/fish/n8n/default.nix
+++ home-manager/fish/n8n/default.nix
@@ -22,7 +22,21 @@ in
 {
   home.file = {
     ".n8n/init-data.sh".text = replaceStrings rawEnv newEnv (readFile ./init-data.sh);
-    ".n8n/docker-compose.yml".text = replaceStrings rawEnv newEnv (readFile ./docker-compose.yml);
+    ".n8n/docker-compose.yml".text =
+      replaceStrings
+        (
+          rawEnv
+          ++ [
+            "\${HOME_DIR}"
+          ]
+        )
+        (
+          newEnv
+          ++ [
+            n8n.homeDir
+          ]
+        )
+        (readFile ./docker-compose.yml);
   };
 
   programs.fish.shellAliases = {
