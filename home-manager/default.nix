5d398cc13ae1de6b775a4f2313b01cd92addde34
HsuTing <hsuting0106@gmail.com>
2025-10-19 07:32:34 +0000

    feat(home manager): add wtfutil

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -74,6 +74,7 @@ with builtins;
               ./somo.nix
               ./tabiew.nix
               ./tree.nix
+              ./wtfutil.nix
               (import ./tmux.nix user)
               (import ./git.nix user)
               (import ./neovim user)
