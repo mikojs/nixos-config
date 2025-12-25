9d45c27289f740beb6dfdab2e59fbf68e31e4240
HsuTing <hsuting0106@gmail.com>
2025-12-25 16:40:57 +0000

    feat(home manager): add docs

--- home-manager/bottom.nix
+++ home-manager/bottom.nix
@@ -3,6 +3,16 @@
   ...
 }:
 {
+  xdg.configFile = {
+    "miko/bottom.md".text = ''
+      # Bottom
+
+      Yet another cross-platform graphical process/system monitor.
+
+      - [Repository](https://github.com/ClementTsang/bottom)
+    '';
+  };
+
   home.packages = with pkgs; [
     bottom
   ];
--- home-manager/fastfetch.nix
+++ home-manager/fastfetch.nix
@@ -3,6 +3,16 @@
   ...
 }:
 {
+  xdg.configFile = {
+    "miko/fastfetch.md".text = ''
+      # Fastfetch
+
+      A maintained, feature-rich and performance oriented, neofetch like system information tool.
+
+      - [Repository](https://github.com/fastfetch-cli/fastfetch)
+    '';
+  };
+
   home.packages = with pkgs; [
     fastfetch
   ];
