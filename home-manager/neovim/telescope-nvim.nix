879f9d30bb73d59ffeb68467f97b581a3ed4242a
HsuTing <hsuting0106@gmail.com>
2025-12-25 19:31:03 +0000

    feat(nvim): add docs commands

--- home-manager/neovim/telescope-nvim.nix
+++ home-manager/neovim/telescope-nvim.nix
@@ -86,7 +86,9 @@
           { "<leader>lT", builtin.lsp_type_definitions, desc = "Go to type definitions" },
           { "<leader>lR", builtin.lsp_references, desc = "Show references" },
           { "<leader>lI", builtin.lsp_implementations, desc = "Go to implementations" },
-          { "<leader>lS", builtin.lsp_document_symbols, desc = "Show document symbols" },
+
+          { "<leader>DF", function() builtin.find_files({ cwd = "~/.docs" }) end, desc = "Find files in the docs directory" },
+          { "<leader>DG", function() builtin.live_grep({ cwd = "~/.docs" }) end, desc = "Search with grep in the docs directory" },
         })
       '';
     }
