7892576d163c9db3f32140319b02c37da5b2972f
HsuTing <hsuting0106@gmail.com>
2025-12-27 12:17:21 +0000

    docs(neovim): add more docs

--- home-manager/neovim/markview-nvim.nix
+++ home-manager/neovim/markview-nvim.nix
@@ -3,6 +3,21 @@
   ...
 }:
 {
+  home.file = {
+    ".docs/neovim/markview-nvim.md".text = ''
+      # Neovim markview.nvim
+
+      Markview.nvim is a plugin for Neovim that provides a mark viewer.
+
+      [Repository](https://github.com/OXY2DEV/markview.nvim)
+
+      ```nvim
+      :Markview ...
+      ```
+
+    '';
+  };
+
   programs.neovim.plugins = with pkgs.vimPlugins; [
     markview-nvim
   ];
