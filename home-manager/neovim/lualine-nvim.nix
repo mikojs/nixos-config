1a83a25389d113e99beca623dd06f497f3ebddd8
HsuTing <hsuting0106@gmail.com>
2025-12-27 10:06:43 +0000

    feat(neovim): add more docs

--- home-manager/neovim/lualine-nvim.nix
+++ home-manager/neovim/lualine-nvim.nix
@@ -3,6 +3,17 @@
   ...
 }:
 {
+  home.file = {
+    ".docs/neovim/lualine-nvim.md".text = ''
+      # Neovim lualine-nvim
+
+      Lualine.nvim is a statusline plugin for Neovim.
+
+      [Repository](https://github.com/nvim-lualine/lualine.nvim)
+
+    '';
+  };
+
   programs.neovim.plugins = with pkgs.vimPlugins; [
     {
       plugin = lualine-nvim;
