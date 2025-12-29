c96802566fd006e2bc6c7520fb5eb0ce90a95f68
HsuTing <hsuting0106@gmail.com>
2025-12-29 18:48:57 +0000

    feat(home manager): move custom commands docs

--- home-manager/fish/custom.nix
+++ home-manager/fish/custom.nix
@@ -4,10 +4,39 @@
 }:
 with pkgs;
 {
-  home.packages = [
-    miko-initialize
-    miko-coder
-  ];
+  home = {
+    file = {
+      ".docs/initialize.md".text = ''
+        # [miko] Initialize
+
+        Initialize the system with custom settings.
+
+        [Code](https://github.com/mikojs/nixos-config/tree/main/overlays/custom/initialize)
+
+        ## Support packages
+
+        - `tide`
+        - `gh`
+        - `tailscale`
+
+      '';
+
+      ".docs/coder.md".text = ''
+        # [miko] Coder
+
+        Some helpful commands to sync code between machines.
+        Use `--help` to see available commands.
+
+        [Code](https://github.com/mikojs/nixos-config/tree/main/overlays/custom/coder)
+
+      '';
+    };
+
+    packages = [
+      miko-initialize
+      miko-coder
+    ];
+  };
 
   programs.fish.interactiveShellInit = miko-fish.interactiveShellInit;
 }
--- home-manager/languages/db.nix
+++ home-manager/languages/db.nix
@@ -3,7 +3,19 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    miko-db
-  ];
+  home = {
+    file.".docs/coder.md".text = ''
+      # [miko] DB
+
+      Some helpful commands for working with databases.
+      Use `--help` to see available commands.
+
+      [Code](https://github.com/mikojs/nixos-config/tree/main/overlays/custom/db)
+
+    '';
+
+    packages = with pkgs; [
+      miko-db
+    ];
+  };
 }
