6326156309cbf0cfeae3db68a4be9ee7c843ce0a
HsuTing <hsuting0106@gmail.com>
2026-01-13 19:55:11 +0000

    fix(n8n): fix error

--- home-manager/fish/n8n/default.nix
+++ home-manager/fish/n8n/default.nix
@@ -19,7 +19,7 @@ let
     n8n.postgresDb
     n8n.postgresNonRootUser
     n8n.postgresNonRootPassword
-    ((elemAt timezones 0) || "America/New_York")
+    (if length timezones <= 0 then "America/New_York" else timezones [ 0 ])
   ];
 in
 {
