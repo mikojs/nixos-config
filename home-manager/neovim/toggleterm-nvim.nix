78b4d251f6980cba6bbd2045a603083d11881469
HsuTing <hsuting0106@gmail.com>
2026-02-20 14:57:24 +0000

    feat(neovim): add new command for toggleterm

--- home-manager/neovim/toggleterm-nvim.nix
+++ home-manager/neovim/toggleterm-nvim.nix
@@ -12,13 +12,18 @@
 
     ## Keybindings
 
-    | Description                | Key          |
-    | ---                        | ---          |
-    | Toggle terminal            | `<leader>Tn` |
-    | Toggle vertical terminal   | `<leader>Tv` |
-    | Toggle horizontal terminal | `<leader>Th` |
-    | Toggle tab terminal        | `<leader>Tt` |
-    | Send selection to terminal | `<leader>st`  |
+    | Description                    | Key           |
+    | ---                            | ---           |
+    | Toggle terminal                | `<leader>Tn`  |
+    | Toggle vertical terminal       | `<leader>Tv`  |
+    | Toggle horizontal terminal     | `<leader>Th`  |
+    | Toggle tab terminal            | `<leader>Tt`  |
+    | Toggle new terminal            | `<leader>Tan` |
+    | Toggle new vertical terminal   | `<leader>Tav` |
+    | Toggle new horizontal terminal | `<leader>Tah` |
+    | Toggle new tab terminal        | `<leader>Tat` |
+    | Select terminal                | `<leader>Ts`  |
+    | Send selection to terminal     | `<leader>st`  |
 
   '';
 
@@ -39,6 +44,11 @@
           { "<leader>Tv", "<Cmd>ToggleTerm direction=vertical size=60<CR>", desc = "Toggle vertical terminal" },
           { "<leader>Th", "<Cmd>ToggleTerm direction=horizontal size=10<CR>", desc = "Toggle horizontal terminal" },
           { "<leader>Tt", "<Cmd>ToggleTerm direction=tab<CR>", desc = "Toggle tab terminal" },
+          { "<leader>Tan", "<Cmd>TermNew direction=float<CR>", desc = "Toggle new terminal" },
+          { "<leader>Tav", "<Cmd>TermNew direction=vertical size=60<CR>", desc = "Toggle new vertical terminal" },
+          { "<leader>Tah", "<Cmd>TermNew direction=horizontal size=10<CR>", desc = "Toggle new horizontal terminal" },
+          { "<leader>Tat", "<Cmd>TermNew direction=tab<CR>", desc = "Toggle new tab terminal" },
+          { "<leader>Ts", "<Cmd>TermSelect<CR>", desc = "Select terminal" },
           { "<leader>st", send_selection_to_terminal, desc = "Send selection to terminal", mode = "v" },
           { "<Esc>", "<C-\\><C-n>", desc = "Exit terminal mode", mode = "t" },
         })
