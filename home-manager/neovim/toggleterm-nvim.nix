53dd95f7cdf9136b9dc28881de767eb44739d872
HsuTing <hsuting0106@gmail.com>
2026-02-20 15:48:53 +0000

    feat(neovim): change size of the terminal

--- home-manager/neovim/toggleterm-nvim.nix
+++ home-manager/neovim/toggleterm-nvim.nix
@@ -36,8 +36,8 @@
         toggleterm.setup()
         require("which-key").add({
           { "<leader>Tn", "<Cmd>ToggleTerm direction=float<CR>", desc = "Toggle terminal" },
-          { "<leader>Tv", "<Cmd>ToggleTerm direction=vertical size=60<CR>", desc = "Toggle vertical terminal" },
-          { "<leader>Th", "<Cmd>ToggleTerm direction=horizontal size=10<CR>", desc = "Toggle horizontal terminal" },
+          { "<leader>Tv", "<Cmd>ToggleTerm direction=vertical size=80<CR>", desc = "Toggle vertical terminal" },
+          { "<leader>Th", "<Cmd>ToggleTerm direction=horizontal size=30<CR>", desc = "Toggle horizontal terminal" },
           { "<leader>Tt", "<Cmd>ToggleTerm direction=tab<CR>", desc = "Toggle tab terminal" },
           { "<leader>st", send_selection_to_terminal, desc = "Send selection to terminal", mode = "v" },
           { "<Esc>", "<C-\\><C-n>", desc = "Exit terminal mode", mode = "t" },
