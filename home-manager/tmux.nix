d922f64c811916ca4b92e9d226c37e63ffcdae54
HsuTing <hsuting0106@gmail.com>
2026-02-21 18:32:34 +0000

    fix(tmux): fix config

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -17,21 +17,23 @@
 
   '';
 
-  programs.tmux = {
-    enable = true;
+  programs = {
+    tmux = {
+      enable = true;
 
-    plugins = with pkgs.tmuxPlugins; [
-      nord
-    ];
+      plugins = with pkgs.tmuxPlugins; [
+        nord
+      ];
 
-    programs.fish.interactiveShellInit = "
+      # FIXME: default shell, https://github.com/nix-darwin/nix-darwin/issues/1237
+      extraConfig =
+        if !isMac then "" else "set-option -g default-command /etc/profiles/per-user/${name}/bin/fish";
+    };
+
+    fish.interactiveShellInit = "
       if not set -q LANG
         set -Ux LANG en_US.UTF-8
       end
     ";
-
-    # FIXME: default shell, https://github.com/nix-darwin/nix-darwin/issues/1237
-    extraConfig =
-      if !isMac then "" else "set-option -g default-command /etc/profiles/per-user/${name}/bin/fish";
   };
 }
