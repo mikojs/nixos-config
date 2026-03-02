6d887c835351a712106363461fc9489f7057a1db
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2026-03-03 01:59:41 +0800

    Could customize n8n env (#163)

--- home-manager/fish/n8n/default.nix
+++ home-manager/fish/n8n/default.nix
@@ -1,26 +1,16 @@
 {
+  lib,
+  pkgs,
   n8n,
   timezones,
   ...
 }:
+with lib;
 with builtins;
 let
-  rawEnv = [
-    "\${POSTGRES_USER}"
-    "\${POSTGRES_PASSWORD}"
-    "\${POSTGRES_DB}"
-    "\${POSTGRES_NON_ROOT_USER}"
-    "\${POSTGRES_NON_ROOT_PASSWORD}"
-    "\${GENERIC_TIMEZONE}"
-  ];
-  newEnv = [
-    n8n.postgresUser
-    n8n.postgresPassword
-    n8n.postgresDb
-    n8n.postgresNonRootUser
-    n8n.postgresNonRootPassword
-    (if length timezones <= 0 then "America/New_York" else (elemAt timezones 0))
-  ];
+  data = n8n // {
+    genericTimezone = (if length timezones <= 0 then "America/New_York" else (elemAt timezones 0));
+  };
 in
 {
   fish-alias = [
@@ -28,22 +18,78 @@ in
   ];
 
   home.file = {
-    ".n8n/init-data.sh".text = replaceStrings rawEnv newEnv (readFile ./init-data.sh);
-    ".n8n/docker-compose.yml".text =
+    ".n8n/init-data.sh".text =
       replaceStrings
-        (
-          rawEnv
-          ++ [
-            "\${HOME_DIR}"
+        [
+          "\${POSTGRES_USER}"
+          "\${POSTGRES_PASSWORD}"
+          "\${POSTGRES_DB}"
+          "\${POSTGRES_NON_ROOT_USER}"
+          "\${POSTGRES_NON_ROOT_PASSWORD}"
+        ]
+        [
+          n8n.postgresUser
+          n8n.postgresPassword
+          n8n.postgresDb
+          n8n.postgresNonRootUser
+          n8n.postgresNonRootPassword
+        ]
+        (readFile ./init-data.sh);
+
+    ".n8n/docker-compose.yml".source = (pkgs.formats.yaml { }).generate "docker-compose.yml" {
+      volumes = {
+        db_storage = null;
+        n8n_storage = null;
+      };
+
+      services = {
+        postgres = {
+          image = "postgres:16";
+          restart = "always";
+          environment = [
+            "POSTGRES_USER=${data.postgresUser}"
+            "POSTGRES_PASSWORD=${data.postgresPassword}"
+            "POSTGRES_DB=${data.postgresDb}"
+            "POSTGRES_NON_ROOT_USER=${data.postgresNonRootUser}"
+            "POSTGRES_NON_ROOT_PASSWORD=${data.postgresNonRootPassword}"
+            "GENERIC_TIMEZONE=${data.genericTimezone}"
+          ];
+          volumes = [
+            "db_storage:/var/lib/postgresql/data"
+            "./_init-data.sh:/docker-entrypoint-initdb.d/init-data.sh"
+          ];
+          healthcheck = {
+            test = [
+              "CMD-SHELL"
+              "pg_isready -h localhost -U ${data.postgresUser} -d ${data.postgresDb}"
+            ];
+            interval = "5s";
+            timeout = "5s";
+            retries = 10;
+          };
+        };
+
+        n8n = {
+          image = "docker.n8n.io/n8nio/n8n";
+          restart = "always";
+          environment = [
+            "DB_TYPE=postgresdb"
+            "DB_POSTGRESDB_HOST=postgres"
+            "DB_POSTGRESDB_PORT=5432"
+            "DB_POSTGRESDB_DATABASE=${data.postgresDb}"
+            "DB_POSTGRESDB_USER=${data.postgresNonRootUser}"
+            "DB_POSTGRESDB_PASSWORD=${data.postgresNonRootPassword}"
           ]
-        )
-        (
-          newEnv
-          ++ [
-            n8n.homeDir
-          ]
-        )
-        (readFile ./docker-compose.yml);
+          ++ (optionals (hasAttr "environment" n8n) n8n.environment);
+          ports = [ "5678:5678" ];
+          links = [ "postgres" ];
+          volumes = [
+            "n8n_storage:/home/node/.n8n"
+          ];
+          depends_on.postgres.condition = "service_healthy";
+        };
+      };
+    };
   };
 
   programs.fish.shellAliases = {
