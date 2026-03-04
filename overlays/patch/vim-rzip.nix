d44382c9ce629e2fddcd092da6dfdf78088bab47
HsuTing <hsuting0106@gmail.com>
2026-03-04 19:16:39 +0000

    fix(vim-rzip): fix package

--- overlays/patch/vim-rzip.nix
+++ overlays/patch/vim-rzip.nix
@@ -4,14 +4,14 @@
   ...
 }:
 vimUtils.buildVimPlugin {
-  pname = "sqls.nvim";
-  version = "2025-03-27";
+  pname = "vim-rzip";
+  version = "2023-02-18";
   src = fetchFromGitHub {
-    owner = "nanotee";
-    repo = "sqls.nvim";
-    rev = "af156c2c7d6bca6384d0881f4a3d6de4cedcd482";
+    owner = "lbrayner";
+    repo = "vim-rzip";
+    rev = "f65400fed27b27c7cff7ef8d428c4e5ff749bf28";
     sha256 = "sha256-D5k8rAwC1vpdbm0MBsJ9vc2vs2Mq3e8ZTtHDycw2cYs=";
   };
-  meta.homepage = "https://github.com/nanotee/sqls.nvim/";
+  meta.homepage = "https://github.com/lbrayner/vim-rzip/";
   meta.hydraPlatforms = [ ];
 }
