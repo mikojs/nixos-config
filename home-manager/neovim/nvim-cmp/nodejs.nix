715fd433fbbcf942c6855cb851ba4b3d86e0266d
HsuTing <hsuting0106@gmail.com>
2026-03-06 20:04:32 +0000

    fix(eslint): fix pnp problem

--- home-manager/neovim/nvim-cmp/nodejs.nix
+++ home-manager/neovim/nvim-cmp/nodejs.nix
@@ -9,14 +9,23 @@
     vscode-langservers-extracted
   ];
 
+  # FIXME: https://github.com/neovim/nvim-lspconfig/issues/3858
   config = ''
     vim.lsp.config("ts_ls", {
       capabilities = capabilities,
     })
     vim.lsp.enable("ts_ls")
 
+    local yarn_lib = vim.fs.find('.yarn/sdks/eslint', { upward = true })[1]
+    local nodePath = ""
+
+    if yarn_lib then
+      nodePath = yarn_lib
+    end
+
     vim.lsp.config("eslint", {
       capabilities = capabilities,
+      settings = { nodePath },
     })
     vim.lsp.enable("eslint")
   '';
