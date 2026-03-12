6c6d8821d1fa91d6ab8d31e584723d2beb6a32fe
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2026-03-12 22:48:59 +0800

    Use serie patch (#170)

--- overlays/patch/default.nix
+++ overlays/patch/default.nix
@@ -10,5 +10,8 @@ with prev.vimUtils;
 
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/vim/plugins/generated.nix#L11618
     nvim-lspconfig = import ./nvim-lspconfig.nix prev;
+
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/se/serie/package.nix
+    serie = import ./serie.nix prev;
   };
 }
