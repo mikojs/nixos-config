3ff3cb6234dbdeb3934b290ad4088a7b05502e1d
HsuTing <hsuting0106@gmail.com>
2026-03-18 03:56:46 +0000

    fix(patch): fix patch

--- overlays/patch/default.nix
+++ overlays/patch/default.nix
@@ -10,10 +10,10 @@ with prev.vimUtils;
 
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/vim/plugins/generated.nix#L11618
     nvim-lspconfig = import ./nvim-lspconfig.nix prev;
-
-    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/se/serie/package.nix
-    serie = import ./serie.nix prev;
   };
+
+  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/se/serie/package.nix
+  serie = import ./serie.nix prev;
 
   # https://github.com/NixOS/nixpkgs/tree/nixos-unstable/pkgs/by-name/cl/claude-code
   claude-code = import ./claude-code prev;
