61fd68d138c5c7824b1d8f28a7ccb94a60cf841f
HsuTing <hsuting0106@gmail.com>
2026-03-19 01:35:23 +0000

    feat(agents): add ac command

--- home-manager/fish/agents.nix
+++ home-manager/fish/agents.nix
@@ -6,6 +6,7 @@ with builtins;
 {
   fish-alias = [
     "- `aa`: Run the agent in the agent folder."
+    "- `ac`: Copy the agent file to the target folder."
   ];
 
   programs.fish.interactiveShellInit = ''
@@ -26,6 +27,14 @@ with builtins;
         set -l info (string split '@' $agent)
 
         complete -c aa -f -n "__fish_use_subcommand" -a $agent -d "Run the $info[1] as $info[2] agent."
+      end
+
+      set -l root_dir $(realpath ~/.agents)
+
+      for file_path in $(find_files ~/.agents)
+        set -l relative_path (string replace "$root_dir/" "" $file_path)
+
+        complete -c ac -f -n "__fish_use_subcommand" -a $relative_path
       end
     end
 
@@ -49,6 +58,21 @@ with builtins;
       cd ~/.agents/$info[2]
       $info[1] $argv[2..-1]
       cd $current
+    end
+
+    function ac --description "ac <source> <target folder>"
+      set -l source_path ~/.agents/$argv[1]
+      set -l target_path argv[2]
+
+      set -l file_name (string split '/' $source_path)[-1]
+      set -l file_extension (string split '.' $file_name)[-1]
+
+      switch $file_extension
+        case "md"
+          cat $source_path > $target_path/$file_name
+        case "json"
+          cat $source_path | jq . > $target_path/$file_name
+      end
     end
   '';
 }
