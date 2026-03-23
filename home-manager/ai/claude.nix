cb8152dc831bae99cc377b31c1e71686d4b24166
HsuTing <hsuting0106@gmail.com>
2026-03-23 02:50:47 +0000

    feat(ai): add claude statusline

--- home-manager/ai/claude.nix
+++ home-manager/ai/claude.nix
@@ -3,20 +3,34 @@
   miko,
   ...
 }:
+with builtins;
 {
   home = {
-    file = miko.getDocs [
-      {
-        filePath = "ai/claude-code";
-        docs = ''
-          # Claude Code
+    file =
+      miko.getDocs [
+        {
+          filePath = "ai/claude-code";
+          docs = ''
+            # Claude Code
 
-          Claude Code is an AI agent that brings the power of Claude directly into your code.
+            Claude Code is an AI agent that brings the power of Claude directly into your code.
 
-          [Repository](https://github.com/anthropics/claude-code)
-        '';
-      }
-    ];
+            [Repository](https://github.com/anthropics/claude-code)
+          '';
+        }
+      ]
+      // {
+        ".claude/settings.json".text = toJSON {
+          "statusLine" = {
+            "type" = "command";
+            "command" = "~/.claude/claude-statusline.fish";
+          };
+        };
+        ".claude/claude-statusline.fish" = {
+          text = (readFile ./claude-statusline.fish);
+          executable = true;
+        };
+      };
 
     packages = with pkgs; [
       claude-code
