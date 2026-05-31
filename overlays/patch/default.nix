1e0dec95f94b250541133980505ad81af04b3bce
HsuTing <hsuting0106@gmail.com>
2026-05-31 17:37:02 +0000

    feat(nixos config): remove patch

--- overlays/patch/default.nix
+++ overlays/patch/default.nix
@@ -5,21 +5,5 @@ with prev.vimUtils;
   vimPlugins = vimPlugins // {
     sqls-nvim = import ./sqls-nvim.nix prev;
     vim-rzip = import ./vim-rzip.nix prev;
-
-    # FIXME: those are in unstable nixpkgs
-
-    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/vim/plugins/generated.nix#L12979-L12991
-    nvim-lspconfig = import ./nvim-lspconfig.nix prev;
   };
-
-  # FIXME: those are in unstable nixpkgs
-
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/se/serie/package.nix
-  serie = import ./serie.nix prev;
-
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/cl/claude-code/package.nix
-  claude-code = import ./claude-code prev;
-
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ge/gemini-cli-bin/package.nix
-  gemini-cli-bin = import ./gemini-cli-bin.nix prev;
 }
