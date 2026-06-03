4e32ea2923b3edd6ed21ca8524b7891fa41c41ad
HsuTing <hsuting0106@gmail.com>
2026-06-03 19:57:47 +0000

    feat(neovim): change style

--- home-manager/neovim/nvim-cmp/default.nix
+++ home-manager/neovim/nvim-cmp/default.nix
@@ -113,8 +113,8 @@ in
               end,
             },
             window = {
-              completion = cmp.config.window.bordered(),
-              documentation = cmp.config.window.bordered(),
+              completion = cmp.config.window.bordered({ border = 'rounded' }),
+              documentation = cmp.config.window.bordered({ border = 'rounded' }),
             },
             mapping = cmp.mapping.preset.insert({
               ["<C-b>"] = cmp.mapping.scroll_docs(-4),
