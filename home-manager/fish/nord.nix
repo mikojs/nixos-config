3cdf053b868837af43901c3d5a4498f10aae966c
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2026-06-04 16:18:53 +0800

    Fix fish upgrade (#195)

--- home-manager/fish/nord.nix
+++ home-manager/fish/nord.nix
@@ -23,25 +23,32 @@
 
     set fish_color_normal $nord4
     set fish_color_command $nord9
+    set fish_color_keyword $nord9
     set fish_color_quote $nord14
     set fish_color_redirection $nord9
     set fish_color_end $nord6
     set fish_color_error $nord11
     set fish_color_param $nord4
+    set fish_color_option $nord4
     set fish_color_comment $nord3
-    set fish_color_match $nord8
     set fish_color_search_match $nord8
     set fish_color_operator $nord9
     set fish_color_escape $nord13
     set fish_color_cwd $nord8
+    set fish_color_cwd_root $nord11
     set fish_color_autosuggestion $nord6
     set fish_color_user $nord4
     set fish_color_host $nord9
+    set fish_color_host_remote $nord13
+    set fish_color_history_current --bold
+    set fish_color_valid_path --underline
+    set fish_color_selection white --background=brblack --bold
+    set fish_color_status $nord11
     set fish_color_cancel $nord15
     set fish_pager_color_prefix $nord13
     set fish_pager_color_completion $nord6
     set fish_pager_color_description $nord10
     set fish_pager_color_progress $nord12
-    set fish_pager_color_secondary $nord1
+    set fish_pager_color_selected_background -r
   '';
 }
