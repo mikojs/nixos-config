4afb52933a477048d172ab3c9c1498a61a7101b2
HsuTing <hsuting0106@gmail.com>
2026-06-11 02:07:58 +0000

    feat(tmux): add new tm command

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -40,8 +40,57 @@
         set -Ux LANG en_US.UTF-8
       end
 
+      # Run a command in tmux
+      function tm --description "tm <command> [-s session_name]"
+        argparse 's/session=' -- $argv
+        or return 1
+
+        if test (count $argv) -eq 0
+          echo "❌ Error: Please provide a command to run!"
+          echo "Usage: tm <command> [-s session_name]"
+          return 1
+        end
+
+        if set -q _flag_session
+          if set -q TMUX
+            if test "$_flag_session" = (tmux display-message -p '#{session_name}')
+              echo "⚡ Already inside [$_flag_session]. Executing command directly..."
+              eval $argv
+            else
+              if tmux has-session -t $_flag_session 2>/dev/null
+                echo "🔄 Switching to existing session [$_flag_session] and splitting pane..."
+                tmux split-window -t $_flag_session -h "$argv; exec $SHELL"
+              else
+                echo "🆕 Session [$_flag_session] not found. Creating it automatically..."
+                tmux new-session -d -s $_flag_session "$argv; exec $SHELL"
+              end
+
+              tmux switch-client -t $_flag_session
+            end
+          else
+            if tmux has-session -t $_flag_session 2>/dev/null
+              echo "🔄 Session [$_flag_session] exists. Splitting pane and attaching..."
+              tmux split-window -t $_flag_session -h "$argv; exec $SHELL"
+              tmux attach-session -t $_flag_session
+            else
+              echo "🆕 Session [$_flag_session] not found. Creating and attaching..."
+              tmux new-session -s $_flag_session "$argv; exec $SHELL"
+            end
+          end
+
+        else
+          if set -q TMUX
+            echo "⚡ Inside tmux. Executing command directly..."
+            eval $argv
+          else
+            echo "🆕 Creating a new default tmux session and executing..."
+            tmux new-session "$argv; exec $SHELL"
+          end
+        end
+      end
+
       # Show tmux panes
-      function times --description "Show tmux panes"
+      function tmls --description "Show tmux panes"
         begin
           echo -e "session,pane,command"
           tmux list-panes -a -F "#{session_name},#{window_index}.#{pane_index},#{pane_current_command}"
