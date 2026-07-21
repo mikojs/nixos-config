c7c0c113368986e5ea2027b93e273371e8df3cee
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2026-07-21 09:25:44 +0800

    Fix kitty scroll problem (#214)

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,7 +8,7 @@
   _module.args.miko = import ./miko.nix { inherit lib pkgs; };
 
   nixpkgs.overlays = [
-    inputs.llm-agents.overlays.default
+    inputs.llm-agents.overlays.shared-nixpkgs
     inputs.miko-db.overlays.default
     inputs.miko-coder.overlays.default
     (import ./patch)
