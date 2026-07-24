b5f7fce7cbc0b7a7968c74314c54d01ea7517df9
HsuTing <hsuting0106@gmail.com>
2026-07-24 17:43:34 +0000

    feat(nvim-cmp): add new SqlsLoad to dynamic load the connections

--- home-manager/neovim/nvim-cmp/db.nix
+++ home-manager/neovim/nvim-cmp/db.nix
@@ -31,18 +31,51 @@
   ];
 
   config = ''
-    local db_sqls_command = io.popen("db sqls")
-    local connections = vim.json.decode(db_sqls_command:read("*a"))
+    local sqls_connections = {}
 
-    db_sqls_command:close()
-    vim.lsp.config("sqls", {
-      capabilities = capabilities,
-      settings = {
-        sqls = {
-          connections = connections,
-        },
-      },
-    })
+    vim.api.nvim_create_user_command("SqlsLoad", function(opts)
+      local alias = opts.fargs[1]
+      local cmd = table.concat(opts.fargs, " ", 2)
+      local url = vim.fn.system(cmd)
+
+      if vim.v.shell_error ~= 0 then
+        vim.notify("SqlsLoad failed: " .. url, vim.log.levels.ERROR)
+
+        return
+      end
+
+      url = url:gsub("%s+$", "")
+
+      local scheme = url:match("^(%w+)://")
+      local driver_map = {
+        postgresql = "postgresql",
+        postgres = "postgresql",
+        mysql = "mysql",
+        sqlite = "sqlite3",
+        sqlserver = "mssql",
+      }
+      local found = false
+
+      for i, conn in ipairs(sqls_connections) do
+        if conn.alias == alias then
+          sqls_connections[i] = { alias = alias, driver = driver_map[scheme] or scheme, dataSourceName = url }
+          found = true
+
+          break
+        end
+      end
+
+      if not found then
+        table.insert(sqls_connections, { alias = alias, driver = driver_map[scheme] or scheme, dataSourceName = url })
+      end
+
+      vim.lsp.config("sqls", {
+        capabilities = capabilities,
+        settings = { sqls = { connections = sqls_connections } },
+      })
+      vim.cmd("LspRestart sqls")
+    end, { nargs = "+" })
+
     vim.lsp.enable("sqls")
   '';
 }
