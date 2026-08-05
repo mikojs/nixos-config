61e9d0a8a18e22064c8f944f7368e044c69f7aef
HsuTing <hsuting0106@gmail.com>
2026-08-06 01:20:23 +0800

    feat(ai): add docker-sbx

--- home-manager/ai/default.nix
+++ home-manager/ai/default.nix
@@ -87,10 +87,30 @@ if !useAI then
 else
   {
     home = {
-      file = getConfig [
-        "home"
-        "file"
-      ] { };
+      file =
+        getConfig
+          [
+            "home"
+            "file"
+          ]
+          (
+            miko.getDocs [
+              {
+                filePath = "docker-sbx";
+                docs = ''
+                  # Docker SBX
+
+                  Safe environments for agents. Docker Sandboxes delivers isolated
+                  environments with restricted filesystem, network, and tool access,
+                  enabling autonomous agent operation while protecting your system and data.
+
+                  The main program is `sbx`.
+
+                  [Repository](https://docs.docker.com/reference/cli/sbx/)
+                '';
+              }
+            ]
+          );
 
       packages =
         getConfig
@@ -98,7 +118,7 @@ else
             "home"
             "packages"
           ]
-          [ ];
+          [ pkgs.docker-sbx ];
     };
 
     programs.fish.interactiveShellInit = ''
