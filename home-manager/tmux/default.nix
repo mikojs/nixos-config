bda275fd786fcaf13ce9f4e4d79cb7f20d355ada
HsuTing <hsuting0106@gmail.com>
2026-08-16 03:06:28 +0000

    feat(tmux): add tmux file

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -8,6 +8,11 @@
   isMac,
   ...
 }:
+let
+  getConfig = miko.getConfig [
+    ./tm.nix
+  ] { inherit pkgs miko; };
+in
 {
   home.file = miko.getDocs [
     {
@@ -18,6 +23,9 @@
         Tmux is a terminal multiplexer.
 
         [Repository](https://github.com/tmux/tmux)
+
+        # Alias
+        ${getConfig [ "fish-alias" ] ""}
       '';
     }
   ];
@@ -40,76 +48,7 @@
         set -Ux LANG en_US.UTF-8
       end
 
-      # Run a command in tmux
-      function tm --description "tm <command> [-s session_name] — run a command in a tmux session"
-        argparse 's/session=' -- $argv
-        or return 1
-
-        if test (count $argv) -eq 0
-          echo "❌ Error: Please provide a command to run!"
-          echo "Usage: tm <command> [-s session_name]"
-          return 1
-        end
-
-        if set -q _flag_session
-          if set -q TMUX
-            if test "$_flag_session" = (tmux display-message -p '#{session_name}')
-              echo "⚡ Already inside [$_flag_session]. Executing command directly..."
-              eval $argv
-            else
-              if tmux has-session -t $_flag_session 2>/dev/null
-                echo "🔄 Switching to existing session [$_flag_session] and creating new window..."
-                tmux new-window -t $_flag_session "$argv; exec $SHELL"
-              else
-                echo "🆕 Session [$_flag_session] not found. Creating it automatically..."
-                tmux new-session -d -s $_flag_session "$argv; exec $SHELL"
-              end
-
-              tmux switch-client -t $_flag_session
-            end
-          else
-            if tmux has-session -t $_flag_session 2>/dev/null
-              echo "🔄 Session [$_flag_session] exists. Creating new window and attaching..."
-              tmux new-window -t $_flag_session "$argv; exec $SHELL"
-              tmux attach-session -t $_flag_session
-            else
-              echo "🆕 Session [$_flag_session] not found. Creating and attaching..."
-              tmux new-session -s $_flag_session "$argv; exec $SHELL"
-            end
-          end
-
-        else
-          if set -q TMUX
-            echo "⚡ Inside tmux. Executing command directly..."
-            eval $argv
-          else
-            set -l git_root (git rev-parse --show-toplevel 2>/dev/null)
-
-            if test -n "$git_root"
-              set -l git_session (basename $git_root)
-              echo "🆕 Git repo detected. Using session [$git_session]..."
-
-              if tmux has-session -t $git_session 2>/dev/null
-                tmux new-window -t $git_session "$argv; exec $SHELL"
-                tmux attach-session -t $git_session
-              else
-                tmux new-session -s $git_session "$argv; exec $SHELL"
-              end
-            else
-              echo "🆕 Creating a new default tmux session and executing..."
-              tmux new-session "$argv; exec $SHELL"
-            end
-          end
-        end
-      end
-
-      # Show tmux panes
-      function tmls --description "tmls — show tmux panes"
-        begin
-          echo -e "session,pane,command"
-          tmux list-panes -a -F "#{session_name},#{window_index}.#{pane_index},#{pane_current_command}"
-        end | column -t -s ','
-      end
+      ${getConfig [ "programs" "fish" "interactiveShellInit" ] ""}
     '';
   };
 }
